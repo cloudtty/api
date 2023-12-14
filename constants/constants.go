20e97d25183c70c64ca846fbf7b0e0c08ccf6345
calvin <wen.chen@daocloud.io>
2023-12-14 16:56:57 +0800

    init cloudtty api repo
    Signed-off-by: calvin <wen.chen@daocloud.io>

---
+++ constants/constants.go
@@ -0,0 +1,18 @@
+package constants
+
+const (
+	DefaultPathPrefix         = "/apis/v1alpha1/cloudshell"
+	DefaultIngressName        = "cloudshell-ingress"
+	DefaultVirtualServiceName = "cloudshell-virtualService"
+	DefaultServicePort        = 7681
+	DefaultTtydImage          = "ghcr.io/cloudtty/cloudshell:v0.6.0"
+
+	CloudshellPodLabelKey = "cloudshell.cloudtty.io/pod-name"
+
+	WorkerOwnerLabelKey        = "worker.cloudtty.io/owner-name"
+	WorkerRequestLabelKey      = "worker.cloudtty.io/request"
+	WorkerBindingCountLabelKey = "worker.cloudtty.io/binding-count"
+	WorkerNameLabelKey         = "worker.cloudtty.io/name"
+
+	PodTemplatePath = "/etc/cloudtty/pod-temp.yaml"
+)
